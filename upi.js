e2369ed74a88600b6ceb9375c6e60253f0ac4085
iasaurav <101176678+iasaurav@users.noreply.github.com>
2025-07-20 16:39:15 +0530

    Update upi.js

--- upi.js
+++ upi.js
@@ -1,4 +1,3 @@
-<script>
+
 const upi = "1koxjuxMbECfSICXAtROqFCMPgz5aO8lJ";
 document.write('<img src="https://lh3.googleusercontent.com/d/' + upi + '" style="max-width:100%;height:auto">');
-</script>
