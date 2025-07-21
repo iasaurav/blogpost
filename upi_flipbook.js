c850662b08afb7afecca7580cfcdb26efe541331
iasaurav <101176678+iasaurav@users.noreply.github.com>
2025-07-21 18:08:30 +0530

    Update upi_flipbook.js

--- upi_flipbook.js
+++ upi_flipbook.js
@@ -1,10 +1,12 @@
 
 
 //<script>
-const banks = [
-  { name: "SBI", id: "1koxjuxMbECfSICXAtROqFCMPgz5aO8lJ" },
-  { name: "KOT", id: "1kopGAfl6g97zRqGv8xu34FAiB0aMe7nA" }
-];
+document.body.innerHTML = `<div id="flipbook" style="text-align:center; max-width:600px; margin:auto; padding:16px; user-select:none;">
+  <div id="bank-name" style="font-size:22px; font-weight:bold; margin-bottom:12px; color:#222;"></div>
+  <div id="image-container" style="width:100%;"></div>
+</div>`;
+
+
 
 let currentIndex = 0;
 const nameBox = document.getElementById("bank-name");
